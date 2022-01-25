dc5d4c2b8b697d8ae9449ca8c6925e4ad1dfb48d
brianthayerinsight <brian.thayer@insight.com>
2022-01-25 12:00:40 -0600

    initial commit

---
+++ popup.js
@@ -0,0 +1,43 @@
+if (navigator.geolocation) {
+    // Call getCurrentPosition with success and failure callbacks
+    navigator.geolocation.getCurrentPosition(findBreweries, currentPositionFailure);
+}
+else {
+    alert("Sorry, your browser does not support geolocation services.");
+}
+
+function findBreweries(position) {
+    fetch(`https://api.openbrewerydb.org/breweries?by_dist=${position.coords.latitude},${position.coords.longitude}`)
+        .then(response => response.json())
+        .then(breweries => {
+            console.log(breweries);
+
+            if (breweries.length && breweries.length > 0) {
+                const listContainer = document.getElementById("brewery-list-container");                
+
+                breweries.forEach(brewery => {
+                    const breweryItem = document.createElement("div");
+                    breweryItem.innerHTML = `
+                    <h3>${brewery.name}</h3>
+                    ${brewery.street}<br>
+                    ${brewery.city}, ${brewery.state} ${brewery.postal_code.split('-')[0]}
+                    <p>${brewery.phone}</p>
+                    <a href="${brewery.website_url}" target="_blank">${brewery.website_url}</a>
+                    <hr/>
+                    `;
+
+                    listContainer.appendChild(breweryItem);
+                });
+            } else {
+                alert("Sorry, no breweries were found near you.");
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            alert("An error occurred while retrieving the list of breweries.");
+        });
+}
+
+function currentPositionFailure() {
+    alert("An error occurred while retrieving your current position.");
+}
